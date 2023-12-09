ea640ebea016df014ac214b6a7959369a32989d3
Yogendra Kokamkar <yogendrak43@gmail.com>
2023-12-09 15:53:24 +0530

    Create github-jira.py

---
+++ github-jira.py
@@ -0,0 +1,57 @@
+from flask import Flask
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+app = Flask(__name__)
+
+@app.route("/createJIRA", methods=['POST'])
+def createJIRA():
+    url = "https://yoursite.atlassian.net/rest/api/3/issue"
+
+    auth = HTTPBasicAuth("email@example.com", "<apitoken>")
+
+    headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+    }
+
+    payload = json.dumps( {
+    "fields": {
+        "description": {
+        "content": [
+            {
+            "content": [
+                {
+                "text": "My First Jira Ticket.",
+                "type": "text"
+                }
+            ],
+            "type": "paragraph"
+            }
+        ],
+        "type": "doc",
+        "version": 1
+        },
+        "issuetype": {
+        "id": "10006"
+        },
+        "project": {
+        "key": "YOG"
+        },
+        "summary": "First JIRA Ticket",
+    },
+    "update": {}
+    } )
+
+    response = requests.request(
+    "POST",
+    url,
+    data=payload,
+    headers=headers,
+    auth=auth
+    )
+
+    return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
+
+app.run('0.0.0.0', port=5000)
